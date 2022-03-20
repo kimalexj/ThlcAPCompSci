45dbffe2a0a6b3a0f0958a3119d2ae20a821d891
Alexander Kim <alaki611@gmail.com>
2022-03-19 21:13:15 -0700

    Fix bugs

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,18 +1,86 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
+import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
-import DrawerLeft from './LeftDrawer';
 import Footer from './Footer';
 import Home from './screens/Home';
 import Today from './screens/Today';
 
 const TABS = ['Home', 'Today', 'Calendar', 'Resources', 'Contact'];
 
+const drawerWidth = 240;
+
+const MainPageWrapper = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
 export default function MainPage() {
+  const theme = useTheme();
   const [currentScreen, changeCurrentScreen] = React.useState('Home');
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   const getScreenContent = () => {
     switch(currentScreen) {
@@ -28,23 +96,71 @@ export default function MainPage() {
   const screen = getScreenContent();
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <Container component="main" sx={{ mt: 8, mb: 2 }}>
-        <DrawerLeft updateScreen={changeCurrentScreen} tabs={TABS}/>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {currentScreen}
-        </Typography>
-        <br />
-        {screen}
-      </Container>
-      <Footer />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {currentScreen}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {TABS.map((text, index) => (
+            <ListItem button key={text} onClick={() => changeCurrentScreen(text)}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <MainPageWrapper open={open}>
+        <DrawerHeader />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+          }}
+        >
+          <CssBaseline />
+          <Container component="main" sx={{ mt: 8, mb: 2 }}>
+            <Typography variant="h5" component="h2">
+              {(currentScreen !== 'Home') ? currentScreen : ''}
+            </Typography>
+            <br />
+            {screen}
+          </Container>
+          <Footer />
+        </Box>
+      </MainPageWrapper>
     </Box>
   );
 }
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -8,18 +8,11 @@ const Home = () => {
             <br/>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
             <Typography variant="h2" component="h1" gutterBottom>
                 Welcome to AP Computer Science
             </Typography>
             <Typography variant="h5" component="h1" gutterBottom>
-                School Year 2021-2022
-            </Typography>
-            <Typography variant="body1" component="h1" gutterBottom>
-                **W.I.P
+                School Year: 2021-2022
             </Typography>
             <br/>
         </div>
