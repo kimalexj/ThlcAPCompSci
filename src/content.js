d03fcf7cb5ae0127e3ffa0f1f4ebcdab9d492153
Alexander Kim <ajk38@cs.washington.edu>
2022-04-04 22:57:57 -0700

    Update materials

--- src/content.js
+++ src/content.js
@@ -11,22 +11,22 @@ const selectImage = () => {
 
 const cardContent = [
     {
-        title: "Today (2016 FRQ)",
-        body: "We'll be going over these questions one by one and breaking down the solutions, scoring, and distribution.",
+        title: "Today (2017 FRQ)",
+        body: "The link will send you to the full exam but we're only going to be covering the multiple choice today.",
         imagePath: selectImage(),
         alt: 'card image',
-        url: 'https://drive.google.com/file/d/1JIqtSNYqlrDUAuXuY_cX6__y2XNeRDHd/view?usp=sharing',
-        urlSecond: 'https://drive.google.com/file/d/126shKOuuB_WWOTyhUySnhMPN1reS-EWF/view?usp=sharing',
+        url: 'https://drive.google.com/file/d/1K0nvw7qiiBTN_Oney5ICFImo5we3MOzE/view?usp=sharing',
+        urlSecond: 'https://drive.google.com/file/d/1dqp5HHsFBOzjdLtMMKO79APSOP-LaQl4/view?usp=sharing',
         hideAnswer: true,
     },
     {
-        title: "Remaining Time (2013)",
-        body: "If we manage to go over all of the questions for the exam set for today, we'll move onto this one for the reaminder of time.",
+        title: "Remaining Time (2013 Q4)",
+        body: "If we manage to go over all of the questions for the exam set for today, we'll move onto this one for the remainder of time.",
         imagePath: selectImage(),
         alt: 'card image',
         url: 'https://drive.google.com/file/d/1ikbpm5lyvLnS49Pr9Dkx5V077UbOy2hb/view?usp=sharing',
         urlSecond: 'https://drive.google.com/file/d/17zld3z8jDERfXHPIvfjymR7gs0QHYYHM/view?usp=sharing',
-        hideAnswer: true,
+        hideAnswer: false,
     }
 ];
 
